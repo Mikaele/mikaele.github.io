8a095378243166b080421c4f283b97db09ebb2d5
MIkaele <mikaele164@gmail.com>
2016-07-15 16:02:55 -0300

    first commit

--- js/script.js
+++ js/script.js
@@ -4,4 +4,19 @@ $(function(){
 	    start: new Date(2011, 8, 24) //year, month, day, hour, min, sec
 	});
 
+	$('#contato-form').validate({
+	    rules: {
+	      name:       {required: true},
+	      email:      {required: true, email: true },
+	      message:    {required: true}
+	    },
+	    highlight: function(element) {
+	      $(element).closest('.form-group').removeClass('success').addClass('error');
+	    },
+	    success: function(element) {
+	      element
+	        .addClass('valid')
+	        .closest('.form-group').removeClass('error').addClass('success');
+	    }
+	  });
 });
